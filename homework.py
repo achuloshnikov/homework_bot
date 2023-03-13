2b7ce629c56b7e8244cbea4001815d303cb7a6d6
Артем Чулошников <a.chuloshnikov@yandex.ru>
2023-03-13 21:13:42 +0300

    Подготовка проекта к сдаче на ревью v1.1

--- homework.py
+++ homework.py
@@ -1,11 +1,19 @@
-...
+import logging
+import os
+import sys
+import time
+from http import HTTPStatus
+
+import requests
+import telegram
+from dotenv import load_dotenv
 
 load_dotenv()
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -15,51 +23,96 @@ HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 HOMEWORK_VERDICTS = {
     'approved': 'Работа проверена: ревьюеру всё понравилось. Ура!',
     'reviewing': 'Работа взята на проверку ревьюером.',
-    'rejected': 'Работа проверена: у ревьюера есть замечания.'
+    'rejected': 'Работа проверена: у ревьюера есть замечания.',
 }
+
+logger = logging.getLogger(__name__)
+
+
+class HTTPStatusError(Exception):
+    """Исключечение для ошибок HTTP статуса."""
+
+    pass
 
 
 def check_tokens():
-    ...
+    """Проверка окружения."""
+    logger.debug('Проверка окружения')
+    if not all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
+        logger.critical('Отсутствует переменная окружения')
+        sys.exit('Программа остановлена')
+    logger.debug('Ошибки окружения отсутствуют')
 
 
 def send_message(bot, message):
-    ...
+    """Отправляет сообщщение в чат тг."""
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+        logger.debug(f'Бот отправил {message}')
+    except Exception as error:
+        logger.error(error)
 
 
 def get_api_answer(timestamp):
-    ...
+    """Запрос к API Практикума с возвратом ответа."""
+    try:
+        payload = {'from_date': timestamp}
+        homework_statuses = requests.get(
+            ENDPOINT, headers=HEADERS, params=payload,
+        )
+        if homework_statuses.status_code != HTTPStatus.OK:
+            raise HTTPStatusError('Ответ не получен')
+        return homework_statuses.json()
+    except requests.exceptions.RequestException as error:
+        raise Exception(f'Ошибка при запросе к API: {error}')
 
 
 def check_response(response):
-    ...
+    """Проверка ответа API."""
+    logging.info('Проверка ответа API')
+    if not isinstance(response, dict):
+        raise TypeError('Не является словарем')
+    homeworks = response.get('homeworks')
+    if not isinstance(homeworks, list):
+        raise TypeError('Не является списком')
+    return homeworks
 
 
 def parse_status(homework):
-    ...
-
+    """Формирует сообщение для отправки."""
+    homework_name = homework.get('homework_name')
+    if homework_name is None:
+        raise KeyError('Нет ключа в словаре')
+    status = homework.get('status')
+    if status not in HOMEWORK_VERDICTS or status is None:
+        raise KeyError(f'Некорректный статус домашки {status}')
+    verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
 def main():
     """Основная логика работы бота."""
-
-    ...
-
+    check_tokens()
+    logger.debug('Запуск бота')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
-
-    ...
-
+    logger.debug('Бот успешно запущен')
+    logger.debug('Определение параметра from_date')
+    timestamp = 0
+    logger.debug('Параметр from_date определен')
     while True:
         try:
-
-            ...
-
+            response = get_api_answer(timestamp)
+            check_response(response)
+            homeworks = response.get('homeworks')
+            homework = homeworks[0]
+            message = parse_status(homework)
+            send_message(bot, message)
         except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+            message = f'Возникновение ошибки: {error}'
+            logger.exception(message)
+            send_message(bot, message)
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
--- tests/conftest.py
+++ tests/conftest.py
@@ -9,20 +9,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 root_dir_content = os.listdir(BASE_DIR)
 HOMEWORK_FILENAME = 'homework.py'
 # проверяем, что в корне репозитория лежит файл с домашкой
-if (
-        HOMEWORK_FILENAME not in root_dir_content
-        or os.path.isdir(os.path.join(BASE_DIR, HOMEWORK_FILENAME))
+if HOMEWORK_FILENAME not in root_dir_content or os.path.isdir(
+    os.path.join(BASE_DIR, HOMEWORK_FILENAME)
 ):
     assert False, (
         f'В директории `{BASE_DIR}` не найден файл '
         f'с домашней работой `{HOMEWORK_FILENAME}`. '
     )
 
-pytest_plugins = [
-    'tests.fixtures.fixture_data'
-]
+pytest_plugins = ['tests.fixtures.fixture_data']
 
 os.environ['PRACTICUM_TOKEN'] = 'sometoken'
 os.environ['TELEGRAM_TOKEN'] = '1234:abcdefg'
 os.environ['TELEGRAM_CHAT_ID'] = '12345'
-
